ee80107383207a4487f910e6b5690929c0ef16d8
Fude-1 <111246727+Fude-1@users.noreply.github.com>
2022-09-15 00:45:12 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#inlude "main.h"
+
+/**
+ * print_numbers - prints numbers 0 to 9 followed by new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+		_putchar ('\n');
+	}
+}
