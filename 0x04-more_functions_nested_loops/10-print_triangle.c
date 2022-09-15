661ae8aa794582a080b7218fa190a296cef4c1a3
Fude-1 <111246727+Fude-1@users.noreply.github.com>
2022-09-15 07:33:44 -0700

    Triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,7 +11,9 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if (size > 0)
+	if (size <= 0)
+		_putchar('\n');
+	else
 	{
 		for (i = 1; i <= size; i++)
 		{
@@ -21,7 +23,7 @@ void print_triangle(int size)
 				_putchar('#');
 			if (i == size)
 				continue;
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
