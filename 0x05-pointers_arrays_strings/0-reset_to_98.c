0ffd2ce4d6eeb31641552d8837a154c621eacead
Fude-1 <111246727+Fude-1@users.noreply.github.com>
2022-09-19 01:07:15 -0700

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer tom an int parameter and updates
+ * the value it point sto 98
+ *
+ * @n: int parameter
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
