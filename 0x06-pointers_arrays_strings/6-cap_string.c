8cc659b05aca72a4275279d6f5331fc9fad747b9
Fude-1 <111246727+Fude-1@users.noreply.github.com>
2022-09-21 22:22:39 -0700

    comit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - Length of a string
- * @s: string
- * Return: returns length as integer
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*(s + len) != '\0')
-		len++;
-
-	return (len);
-}
-
-/**
  * cap_string - Capitalizes all words in a string. First cahracter of a word.
  *
  * @str: String
