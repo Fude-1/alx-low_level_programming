3a8411e7b93aee787a258986be4ad3b465f105f9
Fude-1 <111246727+Fude-1@users.noreply.github.com>
2022-10-03 02:49:11 -0700

    Array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars,
+ *		-and initializes it with a specific char
+ *
+ * @size: Size of array
+ * @c: Character an array is initialized with.
+ *
+ * Return: Returns a pointer to an array.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+
+	s = (char *)malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		s[i] = c;
+
+	return (s);
+}
